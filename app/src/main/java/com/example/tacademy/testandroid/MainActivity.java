2ef8b043235d1caf1990f39decf3ac91dc275993
YongWoonJi <woon013@naver.com>
2016-07-11 14:28:23 +0900

    bbb

--- app/src/main/java/com/example/tacademy/testandroid/MainActivity.java
+++ app/src/main/java/com/example/tacademy/testandroid/MainActivity.java
@@ -59,5 +59,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        btn = (Button) findViewById(R.id.btn_my);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MyActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
